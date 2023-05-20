bc97a45c1130780c55c23021fd84f59b717a0232
5 Etoiles <ebangaarnaud@gmail.com>
2023-05-20 19:02:22 +0100

    Add files via upload
    page d'accueil au projet

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Bienvenue</title>
+	
+</head>
+
+<body>
+	<div class="welcome-container">
+		<h1>Bienvenue sur mon projet</h1>
+		<p>Cliquez sur le bouton ci-dessous pour effectuer des vérifications</p>
+		<a href="predimenssionnement.php" class="button">Continuer</a>
+	</div>
+</body>
+
+</html>
